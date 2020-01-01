caf64209058aff97f3def9a53f1ad5313e07f3d1
Zrondos <zrondos@gmail.com>
2020-01-01 14:11:14 -0800

    Initial Commit

---
+++ AutoLayoutPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutPractice
+//
+//  Created by Zachary Rondos on 1/1/20.
+//  Copyright © 2020 SuhD00DInc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
